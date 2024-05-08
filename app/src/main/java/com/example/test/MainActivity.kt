abdd972b5572a8ae02e39d300c56b02a31e7ae05
Cao Việt Đức <alexcao194@gmail.com>
2024-05-08 08:13:29 -0700

    feat: add entry point to MainApplication

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.test.configs.navigation.AppNavHost
 import com.example.test.ui.theme.TestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
